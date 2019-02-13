2b312c0609f99aefae1365b43ecceb11de9cfeec
Ryan Dulac <analyticrye@gmail.com>
2019-02-13 00:23:21 -0500

    filtering notes working

---
+++ script.js
@@ -0,0 +1,55 @@
+const notes = [
+  {
+    title: "My Next Trip",
+    body: "I would like to go to France"
+  },
+  {
+    title: "Habbits to work on",
+    body: "Exercise. Eating a bit better"
+  },
+  {
+    title: "Socialize",
+    body: "Go to programming meetups"
+  }
+];
+
+const noteP = document.getElementsByClassName("note");
+
+document.getElementById("btn-create").addEventListener("click", () => {
+  console.log("todo created");
+});
+document.getElementById("btn-remove").addEventListener("click", () => {
+  console.log("Remove all");
+  document.querySelectorAll(".note").forEach(note => {
+    note.remove();
+  });
+});
+
+const filters = {
+  searchText: ""
+};
+
+//match notes to filtered notes
+const renderNotes = (notes, filters) => {
+  const filteredNotes = notes.filter(note => {
+    return note.title.toLowerCase().includes(filters.searchText.toLowerCase());
+  });
+  //clear div to get rid of old filtered notes
+  document.getElementById("rootNotes").innerHTML = "";
+
+  //add filtered notes to root
+  filteredNotes.forEach(note => {
+    const noteElement = document.createElement("p");
+    noteElement.textContent = note.title;
+    document.getElementById("rootNotes").appendChild(noteElement);
+  });
+};
+
+//call before user interacts
+renderNotes(notes, filters);
+
+document.getElementById("search-text").addEventListener("input", e => {
+  filters.searchText = e.target.value;
+  //call when user interacts
+  renderNotes(notes, filters);
+});
